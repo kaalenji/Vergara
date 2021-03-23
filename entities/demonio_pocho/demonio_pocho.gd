5af9349d1ee18b556d39b9d34b2c6450a3984643
KuroBS <axgamerman@gmail.com>
2021-03-23 23:13:56 +0100

    Persecución de enemigos
    RUN

--- entities/demonio_pocho/demonio_pocho.gd
+++ entities/demonio_pocho/demonio_pocho.gd
@@ -1,19 +1,19 @@
 extends RigidBody
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export (int) var vida = 500
-
+var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_parent().get_parent().get_node("Player")
+	print(player.name)
 
 func _damaged(var damage):
 	vida -= damage
 	if(vida <= 0):
 		queue_free()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	add_central_force(player.translation - translation)
 #	pass
